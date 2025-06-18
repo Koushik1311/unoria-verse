9b7b254c08a7c4ee996f3d6c6ec6f5b4ecdc401c
Koushik Roy <programmer.koushik@gmail.com>
2025-06-18 22:30:57 +0530

    refactor: remove commented-out QuoteCard code from Home component
    - Deleted the commented-out QuoteCard implementation in the Home component to clean up the code and improve readability.

--- src/app/(base)/q/page.tsx
+++ src/app/(base)/q/page.tsx
@@ -4,21 +4,6 @@ export default function Home() {
   return (
     <div>
       <Quotes />
-      {/* <QuoteCard
-        quote={`<p>I must not fear.</p>
-          <p><strong>Fear is the mind-killer.</strong></p>
-          <p><strong>Fear is the little-death that brings total obliteration.</strong></p>
-          
-          <p>I will face my fear.</p>
-          <p>I will permit it to pass over me and through me.</p>
-          
-          <p><strong>And when it has gone past I will turn the inner eye to see its path.</strong></p>
-          <p><strong>Where the fear has gone there will be nothing.</strong></p>
-          <p><strong>Only I will remain.</strong></p>`}
-        author="Frank Herbert, Dune"
-        type="motivated"
-        keyId="1"
-      /> */}
     </div>
   );
 }
